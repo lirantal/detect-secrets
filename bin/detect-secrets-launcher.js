21a274801a536e3b9641e9ac07c0dc4ba01c9587
Liran Tal <liran.tal@gmail.com>
2019-07-03 00:35:36 +0300

    feat: show warning show warning if unable to detect and spawn any executable from strategies

--- bin/detect-secrets-launcher.js
+++ bin/detect-secrets-launcher.js
@@ -36,10 +36,24 @@ const executableStrategies = [
   }
 ]
 
+let strategiesInvoked = false
 executableStrategies.forEach(strategy => {
   const strategyExists = isExecutableAvailableInPath(strategy.filePath)
   if (strategyExists) {
+    strategiesInvoked = true
     executeStrategy(strategy)
-    process.exit(1)
   }
 })
+
+if (!strategiesInvoked) {
+  console.log(`${pkg.name} ${pkg.version}`)
+  console.log('WARNING: could not execute tool to prevent you from committing secrets')
+  console.log(
+    'to remedy the situation and enable secrets detection in your commits, consider one of:'
+  )
+  console.log(
+    '  1. follow instructions on https://github.com/Yelp/detect-secrets to install detect-secrets'
+  )
+  console.log('  2. install docker and this Node.js CLI will use it to execute an image')
+  console.log('')
+}
